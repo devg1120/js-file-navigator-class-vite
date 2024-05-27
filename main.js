405053b53553d784f7b30dbe888d2c702f6c9b49
unknown <devg1120@gmail.com>
2024-05-27 18:06:53 +0900

    first

---
+++ main.js
@@ -0,0 +1,12 @@
+import "./src/file-tree.js";
+
+import { FileTreeControl } from "./src/fileTreeControl.js";
+
+
+let fc = new FileTreeControl();
+
+// ace editor //
+fc.set_ace_editor();
+
+// monaco editor //
+//fc.set_monaco_editor();

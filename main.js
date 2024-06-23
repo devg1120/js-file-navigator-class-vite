ea10543d9e2504e0ca2ffb0b8dff7b961785a7bc
devg1120 <devg1120@gmail.com>
2024-06-23 13:09:43 +0900

    up

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ import { FileTreeControl } from "./src/fileTreeControl.js";
 let fc = new FileTreeControl();
 
 // ace editor //
-//fc.set_ace_editor();
+fc.set_ace_editor();
 
 
 // monaco editor //
-fc.set_monaco_editor();
+//fc.set_monaco_editor();
